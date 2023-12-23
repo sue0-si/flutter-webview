98ca73d3cf25da0a3fec4b3995f3fd1f7a3815b1
slee3593 <slee3593@gatech.edu>
2023-12-24 02:00:44 +0900

    feat: 종료 확인창

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
@@ -10,6 +11,41 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   late final WebViewController controller;
+
+  void _showBackDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Are you sure?'),
+          content: const Text(
+            'Are you sure you want to leave this app?',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.pop(context);
+                // Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Yes'),
+              onPressed: () async {
+                SystemNavigator.pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   initState() {
@@ -67,8 +103,18 @@ class _MainScreenState extends State<MainScreen> {
           )
         ],
       ),
-      body: WebViewWidget(
-        controller: controller,
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (didPop) async {
+          if(await controller.canGoBack()) {
+            await controller.goBack();
+          } else {
+            _showBackDialog();
+          }
+        },
+        child: WebViewWidget(
+          controller: controller,
+        ),
       ),
     );
   }
