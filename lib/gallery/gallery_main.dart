7690bca0bbb726dede59d3332972f56900609389
slee3593 <slee3593@gatech.edu>
2023-12-25 17:29:46 +0900

    feat: image slide

--- lib/gallery/gallery_main.dart
+++ lib/gallery/gallery_main.dart
@@ -49,25 +49,33 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('gallery'),
-        ),
-        // 기기의 사진을 갖고 와서 메모리에 저장했다가 뿌려줌
-        body: images == null
-            ? const Center(child: Text('No data'))
-        // StreamBuilder로도 가능
-            : FutureBuilder<Uint8List>(
-            future: images![0].readAsBytes(),
-            builder: (context, snapshot) {
-              final data = snapshot.data;
-              if (data == null || snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator(),);
-              }
-              return Image.memory(
-                data,
-                width: double.infinity,
-              );
-            }
-        ));
+      appBar: AppBar(
+        title: const Text('gallery'),
+      ),
+      // 기기의 사진을 갖고 와서 메모리에 저장했다가 뿌려줌
+      body: images == null
+          ? const Center(child: Text('No data'))
+            // 선택된 사진들을 옆으로 넘길 수 있게
+          : PageView(
+              children: images!.map((image) {
+                // StreamBuilder로도 가능
+                return FutureBuilder<Uint8List>(
+                    future: image.readAsBytes(),
+                    builder: (context, snapshot) {
+                      final data = snapshot.data;
+                      if (data == null ||
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return Image.memory(
+                        data,
+                        width: double.infinity,
+                      );
+                    });
+              }).toList(),
+            ),
+    );
   }
 }
